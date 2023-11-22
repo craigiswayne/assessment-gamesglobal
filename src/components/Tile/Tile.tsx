a9488d3348c77d466746993dc746f94e41e8ec67
Craig Wayne <craigiswayne@gmail.com>
2023-11-22 05:52:52 +0000

    Final tweaks

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,7 +3,7 @@ import './Tile.scss';
 export default function Tile({ data }) {
     return (
         <div className='tile'>
-            <img src={data.sites[0].logoSmall2x} />
+            <img src={data.sites[0].logoSmall2x} alt='Tile Image'/>
             <h4>{data.title}</h4>
             <p>{data.shortDescription}</p>
         </div>
